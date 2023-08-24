6494b925ce25f83c9ae564de436095c21a0b6d6f
Your Name <you@example.com>
2023-08-24 11:11:41 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const BtnGenerate = document.querySelector('#BtnGenerate');
+
+
+
+const Quote = async () => {
+const api = `http://api.quotable.io/random`;
+const res = await fetch(api);
+const data = await res.json();
+ const content = document.querySelector('.content h5');
+ const  Author = document.querySelector('.content h6');
+ const tags = document.querySelector('.container h5');
+
+   content.innerHTML = data.content
+   Author.innerHTML = ` <span> Author:</span> ${data.author} `;
+   tags.innerHTML = data.tags
+  }
+
+
+//add event
+BtnGenerate.addEventListener("click", () => {
+    BtnGenerate.classList.add('animate')
+    BtnGenerate.textContent = 'Loading'
+   const Timeout = setTimeout( () => {
+    Quote();
+    BtnGenerate.classList.remove('animate');
+    BtnGenerate.textContent = 'Generate Random Quote';
+   },2000)
+    
+})
+
+
